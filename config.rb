615b6d17f44900e8ed34d81cb33a5c3d2a300093
Colin Bell <CMBell715@users.noreply.github.com>
2015-08-19 09:29:56 -0400

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,9 +1,10 @@
+require 'sass-globbing'
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../stylesheets"
+css_dir = "styles"
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
